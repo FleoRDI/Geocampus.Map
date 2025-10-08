540158eb98b05ce2cb6d2af50d154c90dc839173
FleoRDI <153772146+FleoRDI@users.noreply.github.com>
2025-10-08 15:19:29 +0100

    Ajout d'une page de test pour la carte utilisant Leaflet.

---
+++ Test Web Page/scripts/script1.js
@@ -0,0 +1,10 @@
+function ChangeBG()
+{
+    document.getElementById("map").style.backgroundColor = "pink";
+}
+
+function ResetBG()
+{
+    document.getElementById("map").style.backgroundColor = "transparent";
+}
+
